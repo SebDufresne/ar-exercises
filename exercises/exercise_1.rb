7453ddca34be965e8bd5ac02f1f5e30aa8b43f79
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-08-20 01:49:04 +0000

    Completed exercice

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -16,7 +16,8 @@ puts "----------"
 Store.create([
   { name: 'Burnaby',  annual_revenue: 300000,  mens_apparel: true,  womens_apparel: true},
   { name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true},
-  { name: 'Gastown',  annual_revenue: 190000,  mens_apparel: true,  womens_apparel: false}
+  { name: 'Gastown',  annual_revenue: 190000,  mens_apparel: true,  womens_apparel: false},
+  { name: 'Gastown2',  annual_revenue: 190000,  mens_apparel: false,  womens_apparel: false}
 ])
 
 puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,30 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# ### Exercise 6: One-to-many association
+
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.employees.create([
+  { first_name: "Khurram", last_name: "Virani", hourly_rate: 60 },
+  { first_name: "Dominic", last_name: "Tremblay", hourly_rate: 45},
+  { first_name: "Frederic", last_name: "Bourgouin", hourly_rate: 70}
+])
+
+@store2.employees.create([
+  { first_name: "Cassie", last_name: "Rhéaume", hourly_rate: 60 },
+  { first_name: "Vasiliy", last_name: "Klimkin", hourly_rate: 145},
+  { first_name: "Hanxiang", last_name: "Xu", hourly_rate: 50}
+])
